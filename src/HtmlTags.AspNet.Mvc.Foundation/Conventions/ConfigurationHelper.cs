81ede5811273a0df652dfdb5fb1360781cb6ea53
Craig Smitham <craigsmitham@gmail.com>
2014-08-30 21:45:04 -0500

    Added some missing ConfigurationHelpers

---
+++ src/HtmlTags.AspNet.Mvc.Foundation/Conventions/ConfigurationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlTags.AspNet.Mvc.Foundation.Conventions
+{
+    public static class ConfigurationHelper
+    {
+        public static void UseFoundationConventions(this HtmlTagsConfiguration configuration, Action<FoundationHtmlConventions> conventionConfiguration = null)
+        {
+            configuration.AddConventions(new FoundationHtmlConventions(conventionConfiguration));
+        }
+    }
+}
---
+++ src/HtmlTags.AspNet.Mvc/Conventions/ConfigurationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlTags.AspNet.Mvc.Conventions
+{
+    public static class ConfigurationHelper
+    {
+        public static void UseDefaultHtmlConventions(this HtmlTagsConfiguration configuration, Action<AspNetMvcHtmlConventions> conventionConfiguration = null)
+        {
+            configuration.AddConventions(new AspNetMvcHtmlConventions(conventionConfiguration));
+        }
+    }
+}
